627e6b532944b62dcb8952a1c947ea5ec8155ce8
Neall De Waal <nealldw@gmail.com>
2023-04-28 07:25:31 +0200

    1. Defined project CSS variables. 2. Added app CSS. 3. Fixed export of Containers modules.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
-import { Article, Brand, CTA, Feature, Navbar } from './components'
+// COMPONENTS
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
+import { Brand, CTA, Navbar } from './components'
+
+import './App.css'
 
 const App = () => {
     return (
-        <h1>GPT-3</h1>
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+        </div>
     )
 }
 
-export default App
+export default App;
